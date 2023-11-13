a4b316ed2521cb3ea326ae44ac0e2407892ca058
NurseiitTorobaev <150505078+NurseiitTorobaev@users.noreply.github.com>
2023-11-13 11:57:08 +0600

    Update ConsoleApplication5.cpp

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int A, B, N;
-    cin >> A >> B >> N;
+    cin >> A >> B >> N; //A are soms, B are tyiyns//
     A = A * N + (B * N) / 100;
     B = (B * N) % 100;
     
